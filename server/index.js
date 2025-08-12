49b6a084c74d826ef5b25247a285ffeaa0105499
Saptarshi Ghosh <ghoshsap2612@gmail.com>
2025-08-12 10:30:33 +0530

    add cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const { Server } = require("socket.io");
 require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server);
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  methods: ["GET", "POST"],
+}));
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST"],
+  }
+});
 
 const userSocketMap = {};
 
